4ae2875b9d85a829a2a8ee0149e489b761763e24
shre-byte-11 <114796759+shre-byte-11@users.noreply.github.com>
2022-10-08 12:31:12 +0530

    Create 1st.c

---
+++ 1st.c
@@ -0,0 +1,40 @@
+#include <stdio.h>    
+int main()    
+{    
+    //Initialize array     
+    int arr[] = {2,4, 2, 2, 3, 3, 3, 5 };     
+
+    //Calculate length of array arr    
+    int length = sizeof(arr)/sizeof(arr[0]);    
+
+    //Array fr will store frequencies of element    
+    int fr[length];    
+    int visited = -1;    
+
+    for(int i = 0; i < length; i++){    
+        int count = 1;    
+        for(int j = i+1; j < length; j++){    
+            if(arr[i] == arr[j]){    
+                count++;    
+                //To avoid counting same element again    
+                fr[j] = visited;    
+            }    
+        }    
+        if(fr[i] != visited)    
+            fr[i] = count;    
+    }    
+
+    //Displays the frequency of each element present in array    
+    printf("---------------------\n");    
+    printf(" Element | Frequency\n");    
+    printf("---------------------\n");    
+    for(int i = 0; i < length; i++){    
+        if(fr[i] != visited){    
+            printf("    %d", arr[i]);    
+            printf("    |  ");    
+            printf("  %d\n", fr[i]);    
+        }    
+    }    
+    printf("---------------------\n");    
+    return 0;    
+}
